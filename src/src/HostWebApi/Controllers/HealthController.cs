464a4ff707577e8f25e356a3b4319491f1c3a797
Asier <asiergarciab@outlook.com>
2024-07-14 15:36:16 +0200

    feat:

---
+++ src/src/HostWebApi/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HostWebApi;
+
+[ApiController]
+public class HealthController : Controller
+{
+
+    public HealthController()
+    {
+    }
+
+    [HttpGet("/health")]
+    public IActionResult Health()
+    {
+        return Ok("healthy");
+    }
+
+}
