8ef1bbdd363540ddac8711dbd0055dafa57ee361
Asier <asiergarciab@outlook.com>
2024-07-27 16:09:54 +0200

    Update deployMongo.ps1

--- .docker/deployMongo.ps1
+++ .docker/deployMongo.ps1
@@ -10,6 +10,7 @@ $dockerComposeDeploy = "docker-compose.mongo.yml"
 
 
 # Transfer the image to the VPS
+set -e
 scp -v -i $sshKeyPath -r ./MongoDB "$vpsUser@${vpsHost}:${vpsDest}/"
 scp -i $sshKeyPath $dockerComposeDeploy "$vpsUser@${vpsHost}:${vpsDest}/"
 scp -i $sshKeyPath $envFile "$vpsUser@${vpsHost}:${vpsDest}/"
